2ce41b9011b81d501a9896d7ad0ddebecd1b866c
dmantena-nisum-com <dmantena@nisum.com>
2019-01-15 15:07:33 -0800

    first commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: "Javascript: The Good Parts", pages: 101 },
+    { title: "Harry Potter", pages: 39 },
+    { title: "Node Js", pages: 85 },
+    { title: "Ruby On Rails", pages: 1 }
+  ];
+}
